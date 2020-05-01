8e8cdfe059d3a5ef37af7e5c5d679760369cdcfb
Christopher Straub <ccstraub@gmail.com>
2020-05-01 02:43:53 -0500

    removed view button, modernized page

--- src/components/Saved/Saved.js
+++ src/components/Saved/Saved.js
@@ -4,42 +4,38 @@ import Message from '../Message/Message';
 const Saved = ({ user, messageCode, handleViewBudget, handleAddBudget }) => {
   return (
     <div className="flex justify-center">
-      <div className="window-box mw8">
+      <div className="window-box mw8 w-100">
         {messageCode === 'deleted-budget' ? (
           <Message message="Budget deleted." />
         ) : messageCode === 'created-budget' ? (
           <Message message="Budget created." />
         ) : null}
-        <h1 className="window-title tc mb4">
+        <h1 className="window-title tc mb5">
           {user.name}, you have
           <span className="clr-blue b"> {user.budgets.length}</span> saved
           budgets.
         </h1>
-        <ul className="ul tc">
+        <ul className="ul">
           {user.budgets.length === 0
             ? null
             : user.budgets.map((budget, index) => (
-                <li key={index} className="mv2">
-                  <p className="window-body dib mr3 text-break">
-                    {budget.name}
-                  </p>
-                  <button
-                    onClick={() => handleViewBudget(index)}
-                    className="button bg--blue pv1 ph3"
-                  >
-                    VIEW
-                  </button>
+                <li
+                  key={index}
+                  onClick={() => handleViewBudget(index)}
+                  className="overview-box-title tc text-break dim pointer"
+                >
+                  {budget.name}
                 </li>
               ))}
-          <li>
-            <button
-              onClick={handleAddBudget}
-              className="button bg--green pv1 ph3 mt4"
-            >
-              CREATE NEW BUDGET
-            </button>
-          </li>
         </ul>
+        <div className="flex justify-center">
+          <button
+            onClick={handleAddBudget}
+            className="button bg--green pa3 mt4"
+          >
+            CREATE NEW BUDGET
+          </button>
+        </div>
       </div>
     </div>
   );
