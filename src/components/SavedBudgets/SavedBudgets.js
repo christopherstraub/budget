0be2b6e6d39de566fbce9ef41ccfcc5ba5c1515e
Christopher Straub <ccstraub@gmail.com>
2021-07-29 20:50:11 -0500

    Renamed Budgets component to SavedBudgets

---
+++ src/components/SavedBudgets/SavedBudgets.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import WindowBox from '../WindowBox/WindowBox';
+
+const SavedBudgets = ({
+  user,
+  handleAddBudget,
+  handleViewBudget,
+  handleSaveBudgets,
+  currentBudgetIndex,
+}) => {
+  return (
+    <div className="flex justify-center mt4 ph4">
+      <WindowBox>
+        {user.budgets.length === 0 ? (
+          <h1 className="window-title tc mb5 text-break">
+            {user.displayName}, you don't have any budgets.
+          </h1>
+        ) : user.budgets.length === 1 ? (
+          <h1 className="window-title tc mb4 text-break">
+            {user.displayName}, here's your budget.
+          </h1>
+        ) : (
+          <h1 className="window-title tc mb4 text-break">
+            {user.displayName}, here are your
+            <span className="clr-accent-light b">
+              {' '}
+              {user.budgets.length}
+            </span>{' '}
+            budgets.
+          </h1>
+        )}
+
+        <ul className="list pl0">
+          {user.budgets.length === 0
+            ? null
+            : user.budgets.map((budget, index) => (
+                <li
+                  key={budget.id}
+                  onClick={() => handleViewBudget(index)}
+                  className={`budget-name tc pointer mv4 text-break
+                  ${index === currentBudgetIndex ? 'current-budget' : null}`}
+                >
+                  {budget.name}
+                </li>
+              ))}
+        </ul>
+
+        <div className="flex justify-center pt3">
+          <button
+            onClick={handleAddBudget}
+            className="button bg--accent-dark pv2 ph4 w-33 mr2"
+            style={{ width: 'max-content' }}
+          >
+            NEW BUDGET
+          </button>
+          <button
+            onClick={handleSaveBudgets}
+            className="button bg--green pv2 ph4 w-33 ml2"
+            style={{ width: 'max-content' }}
+          >
+            SAVE BUDGETS
+          </button>
+        </div>
+      </WindowBox>
+    </div>
+  );
+};
+
+export default SavedBudgets;
