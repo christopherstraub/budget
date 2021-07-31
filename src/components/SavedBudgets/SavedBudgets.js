409a95d28e2c1b852712a3a37376ce9af1bebacf
Christopher Straub <ccstraub@gmail.com>
2021-07-31 13:39:21 -0500

    Changed styles

--- src/components/SavedBudgets/SavedBudgets.js
+++ src/components/SavedBudgets/SavedBudgets.js
@@ -10,20 +10,20 @@ const SavedBudgets = ({
   currentBudgetIndex,
 }) => {
   return (
-    <div className="flex justify-center mt4 ph4">
-      <WindowBox>
+    <div className="flex justify-center mt4 ph4 tc">
+      <WindowBox classlist="mw9">
         {user.budgets.length === 0 ? (
-          <h1 className="window-title tc mb5 text-break">
+          <h1 className="clr-light fs-subheading fw3 mb5 text-break">
             {user.displayName}, you don't have any budgets.
           </h1>
         ) : user.budgets.length === 1 ? (
-          <h1 className="window-title tc mb4 text-break">
+          <h1 className="clr-light fs-subheading fw3 mb4 text-break">
             {user.displayName}, here's your budget.
           </h1>
         ) : (
-          <h1 className="window-title tc mb4 text-break">
+          <h1 className="clr-light fs-subheading fw3 mb4 text-break">
             {user.displayName}, here are your
-            <span className="clr-accent-light b">
+            <span className="clr-accent-light fw6">
               {' '}
               {user.budgets.length}
             </span>{' '}
@@ -38,8 +38,10 @@ const SavedBudgets = ({
                 <li
                   key={budget.id}
                   onClick={() => handleViewBudget(index)}
-                  className={`budget-name tc pointer mv4 text-break
-                  ${index === currentBudgetIndex ? 'current-budget' : null}`}
+                  className={`clr-light fs-subtitle fw3 hover-opacity pointer mv4 text-break
+                  ${
+                    index === currentBudgetIndex ? 'clr-accent-light fw6' : ''
+                  }`}
                 >
                   {budget.name}
                 </li>
@@ -49,14 +51,14 @@ const SavedBudgets = ({
         <div className="flex justify-center pt3">
           <button
             onClick={handleAddBudget}
-            className="button bg--accent-dark pv2 ph4 w-33 mr2"
+            className="clr-light fs-body ff-mono selection-transparent hover-opacity fw3 br3 border-0 bg--accent-dark pv2 ph4 w-33 mr2"
             style={{ width: 'max-content' }}
           >
             NEW BUDGET
           </button>
           <button
             onClick={handleSaveBudgets}
-            className="button bg--green pv2 ph4 w-33 ml2"
+            className="clr-light fs-body ff-mono selection-transparent hover-opacity fw3 br3 border-0 bg--green pv2 ph4 w-33 ml2"
             style={{ width: 'max-content' }}
           >
             SAVE BUDGETS
