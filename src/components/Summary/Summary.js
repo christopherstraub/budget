c2fef66f00638fd7769388eb8ab052c60d3e16e1
Christopher Straub <ccstraub@gmail.com>
2021-08-02 00:31:00 -0500

    Changed which values getClassListIfNegative is applied to

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -3,7 +3,7 @@ import React from 'react';
 import EditableLabel from 'react-inline-editing';
 
 const getClassListIfNegative = (value) => {
-  return value < 0 ? 'clr-red' : '';
+  return value < 0 ? 'clr-red fw3' : '';
 };
 
 const Summary = ({
@@ -21,33 +21,21 @@ const Summary = ({
           <h2 className="clr-light-accent fs-subheading fw3 mb0">
             Total Projected Cost
           </h2>
-          <h1
-            className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb5 ${getClassListIfNegative(
-              budget.getProjectedCost()
-            )}`}
-          >
+          <h1 className="clr-light fs-subtitle ff-primary fw3 tr text-break mb5">
             {formattedBudget.projectedCost}
           </h1>
 
           <h2 className="clr-light-accent fs-subheading fw3 mb0">
             Total Actual Cost
           </h2>
-          <h1
-            className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb5 ${getClassListIfNegative(
-              budget.getActualCost()
-            )}`}
-          >
+          <h1 className="clr-light fs-subtitle ff-primary fw3 tr text-break mb5">
             {formattedBudget.actualCost}
           </h1>
 
           <h2 className="clr-light-accent fs-subheading fw3 mb0">
             Total Difference
           </h2>
-          <h1
-            className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb0 ${getClassListIfNegative(
-              budget.getDifferenceCost()
-            )}`}
-          >
+          <h1 className="clr-light fs-subtitle ff-primary fw3 tr text-break mb0">
             {formattedBudget.differenceCost}
           </h1>
         </div>
@@ -66,9 +54,7 @@ const Summary = ({
           <div className="mb5">
             <EditableLabel
               text={formattedBudget.projectedMonthlyIncome}
-              labelClassName={`clr-light fs-subtitle ff-primary fw3 text-break pointer lh-title ${getClassListIfNegative(
-                budget.projectedMonthlyIncome
-              )} ${
+              labelClassName={`clr-light fs-subtitle ff-primary fw3 text-break pointer lh-title ${
                 budget.projectedMonthlyIncome === 0 ? 'empty-number-field' : ''
               }`}
               inputClassName="tr br3 mt2 ph3 w-100"
@@ -89,9 +75,7 @@ const Summary = ({
           </h2>
           <EditableLabel
             text={formattedBudget.actualMonthlyIncome}
-            labelClassName={`clr-light fs-subtitle ff-primary fw3 mb5 text-break pointer lh-title ${getClassListIfNegative(
-              budget.actualMonthlyIncome
-            )} ${
+            labelClassName={`clr-light fs-subtitle ff-primary fw3 mb5 text-break pointer lh-title ${
               budget.actualMonthlyIncome === 0 ? 'empty-number-field' : null
             }`}
             inputClassName="tr br3 mt2 ph3 mb5 w-100"
@@ -105,28 +89,18 @@ const Summary = ({
           <h2 className="clr-light-accent fs-subheading fw3 mb0">
             Projected Balance
           </h2>
-          <h1
-            className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb5 ${getClassListIfNegative(
-              budget.getProjectedBalance()
-            )}`}
-          >
+          <h1 className="clr-light fs-subtitle ff-primary fw3 tr text-break mb5">
             {formattedBudget.projectedBalance}
           </h1>
 
           <h2 className="clr-light-accent fs-subheading fw3 mb0">
             Actual Balance
           </h2>
-          <h1
-            className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb5 ${getClassListIfNegative(
-              budget.getActualBalance()
-            )}`}
-          >
+          <h1 className="clr-light fs-subtitle ff-primary fw3 tr text-break mb5">
             {formattedBudget.actualBalance}
           </h1>
 
-          <h2 className="clr-light-accent fs-subheading fw3 mb0">
-            Balance Difference
-          </h2>
+          <h2 className="clr-light-accent fs-subheading fw3 mb0">Difference</h2>
           <h1
             className={`clr-light fs-subtitle ff-primary fw3 tr text-break mb0 ${getClassListIfNegative(
               budget.getDifferenceBalance()
