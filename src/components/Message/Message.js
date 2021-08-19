d099e11172408d78000d26fde03590248865a70f
Christopher Straub <ccstraub@gmail.com>
2021-08-19 01:43:49 -0500

    Changed styles

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -56,8 +56,11 @@ const getMessage = (code, user, formattedBudget) => {
 
 const Message = ({ code, user, formattedBudget }) => (
   <span
-    className="message fixed pointer-events-none clr-light fs-body bg--window-box tc mb0 pa4 br3 br--top"
-    style={{ zIndex: '999' }}
+    className="message fixed pointer-events-none clr-light fs-body bg--accent-dark tc mb0 pa4 br3 br--top"
+    style={{
+      zIndex: '999',
+      boxShadow: 'rgba(0, 0, 0, 0.56) 0px 22px 70px 4px',
+    }}
   >
     {getMessage(code, user, formattedBudget)}
   </span>
